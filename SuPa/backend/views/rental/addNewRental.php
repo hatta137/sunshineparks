1b215a7ab2b1250fcf24083590eacce43d96db7c
he2754le <hendrik.lendeckel@fh-erfurt.de>
2022-12-22 10:46:18 +0100

    adding new view addNewRental.php

---
+++ SuPa/backend/views/rental/addNewRental.php
@@ -0,0 +1,25 @@
+<section class="newRental">
+    <div class="row">
+        <h1>Folgendes Objekt wurde erfolgreich angelegt:</h1>
+    </div>
+    <div class="row">
+        <ul>
+            <li>Straße:</li>
+            <li>Hausnummer:</li>
+            <li>Postleitzahl:</li>
+            <li>Stadt:</li>
+            <li>Resort:</li>
+            <li>Typ:</li>
+            <li>Maximale Besucherzahl:</li>
+            <li>Badezimmmer:</li>
+            <li>Schlafzimmer:</li>
+            <li>Quadratmeter:</li>
+            <li>Zimmernummer:</li>
+            <li>Etage:</li>
+            <li>Terasse:</li>
+            <li>Balkon:</li>
+            <li>Küchen:</li>
+            <li>Status:</li>
+        </ul>
+    </div>
+</section>
