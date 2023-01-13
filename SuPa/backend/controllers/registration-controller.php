9d6081f015d5da54130699cdb4ad1530cbedd19a
dario <dario.dassler@fh-erfurt.de>
2023-01-13 23:29:49 +0100

    ready to take off logicRegistration

--- SuPa/backend/controllers/registration-controller.php
+++ SuPa/backend/controllers/registration-controller.php
@@ -7,6 +7,6 @@ class RegistrationController extends Controller{
     }
 
     public function logicRegistration(){
-        Person::newPerson("Dario","Daßler","15.10.2001","1234","dario-dassler@gmx.de","A",password_hash("test", PASSWORD_DEFAULT),"street","25",07545,"Leipzig","TH","1");
+        //Person::newPerson("Max","Schelenz","27-09-2001","1234","max.schelenz@fh-erfurt.de","G",password_hash("test", PASSWORD_DEFAULT),"street","25",07545,"Leipzig","TH","7");
     }
 }
