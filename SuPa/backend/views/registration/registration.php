d2bff83710fb74b1b3fd263ed0213022d7b4b47d
dario <dario.dassler@fh-erfurt.de>
2023-01-16 13:11:07 +0100

    fix registration form

--- SuPa/backend/views/registration/registration.php
+++ SuPa/backend/views/registration/registration.php
@@ -9,34 +9,35 @@
 
     <div class="InputZone">
 
-        <form class="InputBox">
+        <div class="InputBox">
             <label>Name</label>
             <input type="text" placeholder="Vorname" name="fname" required>
             <input type="text" placeholder="Nachname" name="lname" required>
-        </form>
+            <input type="date" placeholder="Geburtsdatum" name="birthdate" required>
+        </div>
 
-        <form class="InputBox">
+        <div class="InputBox">
             <label>Adresse</label>
             <input type="text" placeholder="Straße" name="street" required>
             <input type="text" placeholder="Hausnummer" name="housenumber" required>
             <input type="text" placeholder="PLZ" name="zipcode" required>
             <input type="text" placeholder="Ort" name="city" required>
             <input type="text" placeholder="Bundesland*" name="country">
-        </form>
+        </div>
 
-        <form class="InputBox">
+        <div class="InputBox">
             <label>Telefon</label>
             <input type="text" placeholder="Telefon" name="phone" required>
-        </form>
+        </div>
 
-        <form class="InputBox">
+        <div class="InputBox">
             <label>Email Adresse</label>
             <input type="email" placeholder="Email" name="mail" required>
 
             <label>Passwort</label>
             <input type="password" placeholder="Passwort" name="pwd" required>
             <input type="password" placeholder="Passwort wiederholen" name="pwdrepeat" required>
-        </form>
+        </div>
     </div>
 
     <div class="foot">
