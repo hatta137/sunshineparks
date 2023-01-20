6016d959248bf451c2974c2a45ebbc461d6256db
dario <dario.dassler@fh-erfurt.de>
2023-01-20 18:16:18 +0100

    deleteGuest is ready to use

---
+++ SuPa/backend/views/account/deleteGuest.php
@@ -0,0 +1,5 @@
+<div class="message">
+    <h1>Konto löschen?</h1>
+<input type="button" onclick="window.location.href='index.php?page=account&view=deletedPerson'" value="JA!">
+<input type="button" onclick="window.location.href='index.php?page=authentication&view=authenticationGuest'" value="NEIN!">
+</div>
---
+++ SuPa/backend/views/account/deletedPerson.php
@@ -0,0 +1,4 @@
+<div class="message">
+    <h1>Konto erfolgreich gelöscht!</h1>
+<input type="button" onclick="window.location.href='index.php?page=authentication&view=authenticationGuest'" value="Login">
+</div>
