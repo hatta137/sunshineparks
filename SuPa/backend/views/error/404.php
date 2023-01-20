cbcaf2824f717709b015022a74db3cf2faebe0e3
robin_harris <robin.harris@fh-erfurt.de>
2023-01-20 16:19:15 +0100

    styled error messages: 404 and noAcces

--- SuPa/backend/views/error/404.php
+++ SuPa/backend/views/error/404.php
@@ -1,3 +1,4 @@
-<div class="topbox">
+<div class="message">
     <h1>404 Page not found</h1>
+    <input type="button" onclick="window.location.href='index.php?'" value="Zurück zur Startseite">
 </div>
